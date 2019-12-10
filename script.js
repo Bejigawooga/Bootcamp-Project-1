14c63eec0ebf4561740215242a3ea2a423ebf242
locdandloaded <ebony.ashley.brown@gmail.com>
2019-12-09 19:28:51 -0600

    updated htmls for all for pages that added new ids and sample hard code to render gif on last page and sample info on last page

--- script.js
+++ script.js
@@ -10,10 +10,21 @@
 //after all three page choices are made the the 4th page will load a gif that reflects the mood card chosen, a joke from the theme cards, 
 //a song that is from the genre of music chosen and a choice of starting over 
 
+
+
+
+
+var moodObject = JSON.parse(localStorage.getItem("moodObject"));
+
+if (moodObject === null) {
+    moodObject={};
+}
+
 //Event listener for the happy div
-$("#happy").on("click", function() {
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=C61pEa5tz5rNFikLi5NwR6zchVsQVN1j&tag=happy";
+$(".mood").on("click", function() {
+    console.log("click");
+    var mood = $(this).attr("id");
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=C61pEa5tz5rNFikLi5NwR6zchVsQVN1j&tag=" + mood;
 
     $.ajax({
 
@@ -24,33 +35,57 @@ $("#happy").on("click", function() {
 
     .then(function(happyData) {
         console.log(happyData);
-        var imageUrl = happyData.data.image_original_url;
+        moodObject.gifurl = happyData.data.image_original_url;
 
-        var happyImage = $("<img>");
-        
-        happyImage.attr("src", imageUrl);
+       localStorage.setItem("moodObject", JSON.stringify(moodObject));
+    
+      
 
-        happyImage.attr("alt", "Happy Gif");
+    //     $("#total").attr(totalObject);
 
-        $("#happygif").append(happyImage);
-          
-        (function(store){
-       
-            
-        var happyObject_stringify = JSON.stringify(happyImage);
-        console.log(happyObject_stringify);
-    
-        localStorage.setItem("happygif", happyObject_stringify);
-        console.log(localStorage); 
-        
-        console.log(store)
-    
-        }
-        
-    
-    )
+       window.location.href = 'themes.html';
+  
         });
 });
+//this is the code that activates the function when the user clicks a theme to retrive a joke from
+$(".theme").on("click", function() {
+
+// ////below please add ajax from the joke api below
+moodObject.joke = "here's a joke"
+
+// ///this is the code that saves the joke theme choosen by user in local storage
+   localStorage.setItem("moodObject", JSON.stringify(moodObject));
+    
+// ///this is the code that moves the user to the following page
+    window.location.href = 'music.html';
+})
+
+
+  
+  $(".music").on("click", function() {
+
+    moodObject.music = "here's a song";
+
+    localStorage.setItem("moodObject", JSON.stringify(moodObject));
+
+    window.location.href = "finalpage.html"
+  })
+
+  if (window.location.pathname === "/finalpage.html") {
+      console.log("we are on the final page");
+      console.log(moodObject);
+
+
+      $("#gif").attr("src", moodObject.gifurl)
+
+      $("#joke").text(moodObject.joke)
+
+      $("#music").text(moodObject.music)
+
+  }
+console.log(window.location.pathname);
+
+
 
     
 
